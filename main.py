0de1bda20440b9accff9a240ebdea8c4a0f97ab8
Malcolm Vigren <malvi108@student.liu.se>
2016-06-12 10:46:01 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import time
+import Adafruit_CharLCD as LCD
+import RPi.GPIO as GPIO
+from menu import Menu
+
+display = LCD.Adafruit_CharLCDPlate()
+
+DISPLAY_WIDTH = 16
+curr = 0
+
+def button_pressed(channel):
+    display.clear()
+    display.message("Oh shit you\n pressed pin {}".format(channel))
+    time.sleep(3)
+
+time.sleep(1)
+
+BUTTON_PIN = 22
+
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setup(17, GPIO.OUT)
+
+GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=button_pressed)
+
+try:
+    menu = Menu(["Kebab, Mysarna, Mamma"], display, title="KeBaB")
+    menu.display_menu(delay=True)
+except KeyboardInterrupt:
+    display.message("Have a nice\nday!")
+    GPIO.cleanup()
