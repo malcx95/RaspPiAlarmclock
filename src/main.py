a459c48d7a3bdae9e6f0c078b06c39674ec1e7d7
Malcolm Vigren <malvi108@student.liu.se>
2016-08-07 10:53:12 +0200

    Trying to fix not backing problem with global variable

--- src/main.py
+++ src/main.py
@@ -11,41 +11,43 @@ from constants import *
 from menu_elements import *
 
 
-def test():
-    display_lock = threading.Lock()
+# def test():
+#     display_lock = threading.Lock()
+# 
+#     display = Display(display_lock)
+# 
+#     menu = Menu(["Kebab", "Mysarna", "Mamma", "ha", "majs"],
+#                 display, title="MAMMA GILLAR GLASS")
+# 
+#     def button_pressed(channel):
+#         if channel == M1_BUTTON:
+#             menu.move_selection_left()
+#         elif channel == M2_BUTTON:
+#             menu.move_selection_right()
+# 
+#     for button in BUTTONS.values():
+#         GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#         GPIO.add_event_detect(button, GPIO.RISING, callback=button_pressed, 
+#                 bouncetime=300)
+# 
+#     try:
+#         clock = ClockFace(display)
+#         clock.start()
+#         while True:
+#             time.sleep(10)
+#     except KeyboardInterrupt:
+#         display.clear()
+#         display.message("Have a nice\nkebab!")
+#         GPIO.cleanup()
+#         clock.stop()
+#         sys.exit(1)
+# 
+#     menu.stop()
+#     display.clear()
+#     display.message("Exiting")
+#     sys.exit(0)
 
-    display = Display(display_lock)
-
-    menu = Menu(["Kebab", "Mysarna", "Mamma", "ha", "majs"],
-                display, title="MAMMA GILLAR GLASS")
-
-    def button_pressed(channel):
-        if channel == M1_BUTTON:
-            menu.move_selection_left()
-        elif channel == M2_BUTTON:
-            menu.move_selection_right()
-
-    for button in BUTTONS.values():
-        GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(button, GPIO.RISING, callback=button_pressed, 
-                bouncetime=300)
-
-    try:
-        clock = ClockFace(display)
-        clock.start()
-        while True:
-            time.sleep(10)
-    except KeyboardInterrupt:
-        display.clear()
-        display.message("Have a nice\nkebab!")
-        GPIO.cleanup()
-        clock.stop()
-        sys.exit(1)
-
-    menu.stop()
-    display.clear()
-    display.message("Exiting")
-    sys.exit(0)
+back_pressed = False
 
 def main():
 
@@ -75,7 +77,6 @@ def main():
     
     # list of indices tracing the path to the current node
     current_menu_selection = []
-    back_pressed = False
 
     def back(channel):
         main_menu.get_node(current_menu_selection).stop()
