01da1fbf17d9e144285722d5cf22fecaa0976195
Malcolm Vigren <malvi108@student.liu.se>
2016-10-05 22:00:31 +0200

    Removed event detect for alarm app

--- src/alarm_app.py
+++ src/alarm_app.py
@@ -26,7 +26,7 @@ class AlarmApplication(menu_node.MenuNode):
         if self.alarm_list.is_empty():
             self.display.message('No alarms.\nPress {} to add.'\
                                  .format(display.ENTER))
-            self._stop_flag.wait()
+            self._listen_to_input()
             self.lock.acquire()
             if not self._back_pressed:
                 self._add_placeholder_alarm()
@@ -34,15 +34,29 @@ class AlarmApplication(menu_node.MenuNode):
             self.lock.release()
             return None
         else:
-            self._set_up_buttons()
             self._refresh_menu()
-            self._stop_flag.wait()
+            self._listen_to_input()
             self.menu.stop()
             return self._selected
 
     def _get_options(self):
         return [str(al) + ' - ON' if on else str(al) + ' - OFF'
                         for al, on in self.alarm_list]
+    
+    def _listen_to_input(self):
+        while not self._stop_flag.wait(0.1):
+            self.lock.acquire()
+            if GPIO.input(buttons.ENTER):
+                self._enter_pressed()
+            if GPIO.input(buttons.RIGHT):
+                self.menu.move_selection_right()
+            if GPIO.input(buttons.LEFT):
+                self.menu.move_selection_left()
+            if GPIO.input(buttons.SET):
+                self._set_pressed()
+            if GPIO.input(buttons.DELETE):
+                self._delete_pressed()
+            self.lock.release()
     
     def _refresh_menu(self):
         # for alarm, _ in self.alarm_list:
@@ -66,42 +80,12 @@ class AlarmApplication(menu_node.MenuNode):
                     blinking_leds=[self._led_control.ENTER,
                                     self._led_control.SET,
                                     self._led_control.DELETE])
-        #self._set_up_buttons()
         self.menu.display_menu()
-
-    def _set_up_buttons(self):
-
-        def button_pressed(channel):
-            self.lock.acquire()
-            if channel == buttons.LEFT:
-                self.menu.move_selection_left()
-            elif channel == buttons.RIGHT:
-                self.menu.move_selection_right()
-            elif channel == buttons.ENTER:
-                self._enter_pressed()
-            elif channel == buttons.SET:
-                self._set_pressed()
-            elif channel == buttons.DELETE:
-                self._delete_pressed()
-            self._selected = self.menu.get_selected_index()
-            self.lock.release()
-        
-        GPIO.add_event_detect(buttons.ENTER, GPIO.RISING,
-                              callback=button_pressed, bouncetime=300)
-        GPIO.add_event_detect(buttons.RIGHT, GPIO.RISING,
-                              callback=button_pressed, bouncetime=300)
-        GPIO.add_event_detect(buttons.LEFT, GPIO.RISING, 
-                              callback=button_pressed, bouncetime=300)
-        GPIO.add_event_detect(buttons.SET, GPIO.RISING, 
-                              callback=button_pressed, bouncetime=300)
-        GPIO.add_event_detect(buttons.DELETE, GPIO.RISING, 
-                              callback=button_pressed, bouncetime=300)
 
     def _enter_pressed(self):
         self.stop()
 
     def _delete_pressed(self):
-        #self._free_used_buttons()
         self.menu.stop()
         alarm, activated = self.alarm_list[self._selected]
         self.alarm_list.delete_alarm(alarm, activated)
@@ -114,13 +98,6 @@ class AlarmApplication(menu_node.MenuNode):
         self.menu.set_icon_at(icon, self._selected)
         self.menu.update_options(self._get_options())
 
-    def _free_used_buttons(self):
-        GPIO.remove_event_detect(buttons.ENTER)
-        GPIO.remove_event_detect(buttons.RIGHT)
-        GPIO.remove_event_detect(buttons.LEFT)
-        GPIO.remove_event_detect(buttons.SET)
-        GPIO.remove_event_detect(buttons.DELETE)
-
     def _add_placeholder_alarm(self):
         today = datetime.now()
         alarm = alarm.Alarm(7, 0, today.weekday(), 0)
@@ -128,6 +105,9 @@ class AlarmApplication(menu_node.MenuNode):
         editor = AlarmEditor(self.display, self.lock, self._led_control, alarm)
         self.children.append(editor)
         self.alarm_list.add_alarm(alarm, False)
+    
+    def _free_used_buttons(self):
+        pass
     
 
 class AlarmEditor(menu_node.MenuNode):
