ddab626763f1abd134d162a365f474b8cfd672a1
Malcolm Wigren <trekommafem2@gmail.com>
2017-01-23 17:29:17 +0100

    Added simple blink

---
+++ c++/blink.c
@@ -0,0 +1,14 @@
+#include <wiringPi.h>
+
+int main(void) {
+    wiringPiSetup();
+    pinMode(16, OUTPUT);
+
+    while (1) {
+        digitalWrite(16, HIGH);
+        delay(500);
+        digitalWrite(16, LOW);
+        delay(500);
+    }
+    return 0;
+}
