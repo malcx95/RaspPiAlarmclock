e89f3ba1c8b2eae8c3688e759896323f76e443ee
Malcolm Wigren <trekommafem2@gmail.com>
2017-01-27 11:38:49 +0100

    Added error checking to compile script

--- src/compileandrun.sh
+++ src/compileandrun.sh
@@ -6,14 +6,29 @@ if [ ! -d "build" ]; then
 fi
 
 cd build
-echo "Running cmake..."
+echo "Running CMake..."
 cmake -D CMAKE_TOOLCHAIN_FILE=$HOME/raspberrypi/pi.cmake ../
+
+if [ $? -ne 0 ]; then
+    echo "CMake failed"
+    cd ..
+    exit 1
+fi
 
 echo "Compiling..."
 make
+
+if [ $? -ne 0 ]; then
+    echo "Compilation failed"
+    cd ..
+    exit 1
+fi
 
 echo "Uploading..."
 scp clock pi@192.168.1.180:/home/pi/
 
 cd ..
 ssh pi@192.168.1.180 sudo ./clock
+
+echo "Done."
+
