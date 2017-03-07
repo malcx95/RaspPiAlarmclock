d1fe6d2821f4760b3623b6d5e20fbc2caf408ade
Malcolm Wigren <trekommafem2@gmail.com>
2017-03-07 19:17:10 +0100

    Improved simulator display

--- src/tests/simulator/display.py
+++ src/tests/simulator/display.py
@@ -3,7 +3,8 @@ import os
 class SimulatorDisplay(object):
 
     def __init__(self):
-        #self.window = window
+        self.blink_enabled = False
+        self.cursor_enabled = False
         self.char_grid = [[x for x in ' ' * 16], 
                           [x for x in ' ' * 16]]
         self.cursor = (0, 0)
@@ -12,10 +13,10 @@ class SimulatorDisplay(object):
         pass
 
     def blink(self, value):
-        pass
+        self.blink_enabled = value
 
     def show_cursor(self, value):
-        pass
+        self.cursor_enabled = value
 
     def set_cursor(self, col, row):
         self.cursor = (col, row)
@@ -23,13 +24,8 @@ class SimulatorDisplay(object):
     def write8(self, char, char_mode=False):
         col, row = self.cursor
         self.char_grid[row][col] = chr(char)
-        self._update()
 
     def clear(self):
         self.char_grid = [[x for x in '        '], 
                           [x for x in '        ']]
-        self._update()
-
-    def _update(self):
-        os.system("clear")
 
