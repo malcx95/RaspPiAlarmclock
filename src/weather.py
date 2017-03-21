45078326b38180592e2eadf0b4e9641dde7642e1
Malcolm Wigren <trekommafem2@gmail.com>
2017-03-21 19:33:57 +0100

    Weather

--- src/weather.py
+++ src/weather.py
@@ -1,21 +1,27 @@
-import threading
 import xml.etree.ElementTree as ET
+import urllib2
+import xmltodict
+
+URL = "https://www.yr.no/sted/Sverige/%C3%96sterg%C3%B6tland/Link%C3%B6ping/varsel.xml"
+
+
+def update_weather():
+    f = urllib2.urlopen(URL)
+    data = f.read()
+    f.close()
+
+    weather = xmltodict.parse(data)
+
+    print weather['weatherdata']['forecast']['tabular']['time'][0]#['symbol']
 
 
 class WeatherInfo:
 
-    def __init__(self):
-        self._lock = threading.Lock()
-        self.time = None
-        self.weather = None
-        self.precipitation = None
-        self.temperature = None
-        self.refresh()
-
-    def refresh(self):
-        self._lock.acquire()
-        # TODO do something
-        self._lock.release()
+    def __init__(self, time, weather, precipitation, temperature):
+        self.time = time 
+        self.weather = weather 
+        self.precipitation = precipitation
+        self.temperature = temperature 
 
     def __str__(self):
         return 'implement'
